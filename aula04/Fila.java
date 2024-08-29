cfd658627a54e2404a795ac0fdb072abf9a4cb13
waveformer10 <125139631+waveformer10@users.noreply.github.com>
2024-08-29 01:14:19 +0000

    Codigos da aula do dia 28/08/2024

---
+++ aula04/Fila.java
@@ -0,0 +1,56 @@
+public class Fila<T>{
+    private No<T> primeiroNo;
+    private No<T> ultimoNo;
+    private String nomeFila;
+
+    public Fila(){
+        this("Fila");
+    }
+
+    public Fila(String nomeFila){
+        this.nomeFila = nomeFila;
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+    }
+
+    public enfileirar(T dado){
+        No<T> novoNo = new No<T>(dado);
+        if(ultimoNo = null){
+            ultimoNo = primeiroNo = novoNo;
+        }else{
+            ultimoNo.setNextNo(NovoNo);
+            ultimoNo = novoNo;
+        }
+    }
+    
+    public desenfileirar(){
+        if(primeiroNo == null){
+            System.out.println("Fila Vazia");
+            return null;
+        }
+
+        T dado = primeiro.getDado();
+        primeiroNo = primeiroNo.getNextNo();
+
+        if(primeiroNo == null){
+            ultimoNo = null;
+        }
+
+        return dado;
+    }
+
+    public void imprimiFila(){
+        if(primeiroNo == null){
+            System.out.println("Fila vazia!");
+        }else{
+            System.out.println("Dados da fila"+ this.nomefila);
+
+            No<T> aux = primeiroNo;
+
+            while(aux != null){
+                System.out.printf("-%s\n", aux.getDado());
+                aux = aux.getNextNo();
+            }
+        }
+    }
+}
